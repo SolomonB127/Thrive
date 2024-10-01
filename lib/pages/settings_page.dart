c7dae7f24e43d1a9b9f22c9b11eee24010be13be
SolomonB127 <solomonakinsulire@gmail.com>
2024-10-01 16:29:34 +0100

    feat: Created a settings.dart file - Contains logic for switching themes - from light to dark mode

---
+++ lib/pages/settings_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:thrive/theme/theme_provider.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.primary,
+            borderRadius: BorderRadius.circular(8)),
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 25),
+        margin: const EdgeInsets.only(top: 10),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              const Text("Dark Mode"),
+              CupertinoSwitch(
+                  value: Provider.of<ThemeProvider>(context).isDarkMode,
+                  onChanged: (value) =>
+                      Provider.of<ThemeProvider>(context, listen: false)
+                          .toggleTheme())
+            ]),
+      ),
+    );
+  }
+}
