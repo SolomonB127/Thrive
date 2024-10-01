64e0299acd6b30d0e608045c0b9c9b4fb0a32605
SolomonB127 <solomonakinsulire@gmail.com>
2024-10-01 01:26:06 +0100

    feat:Created a themes folder - Contains a light_mode.dart file - light_mode.dart contains colorSchemes for the light theme mode

---
+++ lib/theme/light_mode.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+    colorScheme: ColorScheme.light(
+  brightness: Brightness.light,
+  surface: Colors.grey.shade300,
+  primary: Colors.grey.shade500,
+  secondary: Colors.grey.shade200,
+  tertiary: Colors.white,
+  inversePrimary: Colors.grey.shade900,
+));
