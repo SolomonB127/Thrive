bbe533a7e24c4f7e788bcbc4fbdcc5227b36ec47
SolomonB127 <solomonakinsulire@gmail.com>
2024-10-01 01:33:41 +0100

    feat: Set up ChangeNotifierProvider for theme management
    - Initialized ChangeNotifierProvider to provide ThemeProvider to the app.
    - Wrapped MainApp with ChangeNotifierProvider for theme state management.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:thrive/theme/theme_provider.dart';
+
+import 'pages/home_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeProvider(),
+    child: const MainApp(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
@@ -10,11 +17,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
