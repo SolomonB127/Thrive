9505ec180d527910ec9b9b3110da3a0daef6b80d
SolomonB127 <solomonakinsulire@gmail.com>
2024-10-01 16:26:41 +0100

    fix: Imported in the custom Drawer widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,23 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:thrive/components/drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: Builder(builder: (context) {
+          return IconButton(
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              icon: const Icon(Icons.menu));
+        }),
+      ),
+      drawer: const MyDrawer(),
+    );
   }
 }
