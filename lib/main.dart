4fda6cfdf71aa102e7b5164658eb632198fdc8f9
SolomonB127 <solomonakinsulire@gmail.com>
2024-10-01 01:41:08 +0100

    fix: Apply dynamic theme in MainApp
    - Set the app theme using Provider to access ThemeProvider's themeData.
    - Ensured the current theme updates correctly based on user preference.

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: const HomePage(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
     );
   }
 }
