31694e7e30a44886056dcddc1187fbf006a88051
SolomonB127 <solomonakinsulire@gmail.com>
2024-10-03 23:16:34 +0100

    feat: Implement habit creation functionality in HomePage
    - Added a dialog to create new habits with a text input field.
    - Integrated HabitDatabase to save new habits.
    - Implemented save and cancel actions in the dialog.
    - Initialized habit reading on app startup in initState.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,74 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:thrive/components/drawer.dart';
+import 'package:thrive/database/habit_database.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // read existing habit on app startup
+    Provider.of<HabitDatabase>(context, listen: false).readHabits();
+    super.initState();
+  }
+
+  // Text Controller
+  final textController = TextEditingController();
+  // new habit method
+  void createNewHabit() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              content: TextField(
+                controller: textController,
+                decoration:
+                    const InputDecoration(hintText: "Create a new habit"),
+              ),
+              actions: <Widget>[
+                // save button
+                MaterialButton(
+                  onPressed: () {
+                    // get the new habit name
+                    String newHabitName = textController.text;
+
+                    // save to db
+                    context.read<HabitDatabase>().addNewHabit(newHabitName);
+
+                    // pop box
+                    Navigator.pop(context);
+
+                    // clear controller
+                    textController.clear();
+                  },
+                  child: const Text("Save"),
+                ),
+
+                // cancel button
+                MaterialButton(
+                  onPressed: () {
+                    // pop box
+                    Navigator.pop(context);
+
+                    // clear controller
+                    textController.clear();
+                  },
+                  child: const Text("Cancel"),
+                )
+              ],
+            ));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(
         leading: Builder(builder: (context) {
           return IconButton(
@@ -18,6 +79,12 @@ class HomePage extends StatelessWidget {
         }),
       ),
       drawer: const MyDrawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewHabit,
+        elevation: 0,
+        backgroundColor: Theme.of(context).colorScheme.tertiary,
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
