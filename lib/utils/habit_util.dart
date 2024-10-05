d48abd0835f2050180a943bdfec2a2c338eaaed7
SolomonB127 <solomonakinsulire@gmail.com>
2024-10-05 01:02:03 +0100

    feat: implement prepareHeatMapData function
    - Added prepareHeatMapData function to aggregate habit completion data
    - Normalizes date format to avoid time mismatches
    - Increments count for each completed day in the dataset
    - Returns a Map<DateTime, int> representing habit completion frequency

--- lib/utils/habit_util.dart
+++ lib/utils/habit_util.dart
@@ -2,10 +2,34 @@
   given a habit list of completed days -> is the habit completed today
 */
 
+
+import '../models/habit.dart';
+
 bool isHabitCompletedToday(List<DateTime> completedDays) {
   final today = DateTime.now();
   return completedDays.any((date) =>
       date.year == today.year &&
       date.month == today.month &&
       date.day == today.day);
+}
+
+// Prepare heatmap data set
+Map<DateTime, int> prepareHeatMapData(List<Habit> habits) {
+  Map<DateTime, int> dataSet = {};
+
+  for (var habit in habits) {
+    for (var date in habit.completedDays) {
+      // normalize data to avoid time mismatch
+      final dateTimeFormat = DateTime(date.year, date.month, date.day);
+
+      // if date already exist in dataset, increment count
+      if (dataSet.containsKey(dateTimeFormat)) {
+        dataSet[dateTimeFormat] = dataSet[dateTimeFormat]! + 1;
+      } else {
+        // else initialize with a count of 1
+        dataSet[dateTimeFormat] = 1;
+      }
+    }
+  }
+  return dataSet;
 }
